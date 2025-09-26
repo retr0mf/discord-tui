a2538cb2238df281e6cef7ab0e86b817df63fc94
kowkodivka <86189625+Kowkodivka@users.noreply.github.com>
2025-09-26 13:27:42 +0700

    a

--- src/http/client.rs
+++ src/http/client.rs
@@ -2,6 +2,13 @@ use std::num::NonZeroU64;
 
 use reqwest::Client;
 use secrecy::SecretString;
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Clone, Copy, Serialize, Deserialize)]
+pub struct Message {}
+
+#[derive(Debug, Clone, Copy)]
+pub enum HttpError {}
 
 pub struct Http {
     pub client: Client,
@@ -9,5 +16,11 @@ pub struct Http {
 }
 
 impl Http {
-    pub async fn send_message(self, channel_id: NonZeroU64,) {}
+    pub async fn send_message(
+        self,
+        channel_id: NonZeroU64,
+        content: String,
+    ) -> Result<Message, HttpError> {
+        todo!()
+    }
 }
